480d8c25fab0b3e00e86ff8ed2520f80ca8868c2
Yacine Belarbi <yacbel@gmail.com>
2021-03-10 14:52:49 +0100

    spacing fix

--- wp-content/themes/studioyacine/front-page.php
+++ wp-content/themes/studioyacine/front-page.php
@@ -4,7 +4,12 @@
 
 <main id="" class="main" role="main" itemscope itemprop="mainContentOfPage">
 
-
+	<?php if (get_field('projects_title')) : ?>
+		<div class='SectionHeader'>
+			<strong><?php the_field('projects_title'); ?></strong>
+			<a href="<?php echo get_post_type_archive_link('project'); ?>">All Projects</a>
+		</div>
+	<?php endif; ?>
 	<?php get_template_part_with_params('templates/module', 'teasers', ['id' => 'projects']); ?>
 
 	<?php // get_template_part_with_params('templates/module', 'teasers', ['id' => 'news']); 
